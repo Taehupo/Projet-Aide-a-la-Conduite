3e53266d1d5147837a07d8a763736c8ae9489a8f
Taehupo <uzumakigip@gmail.com>
2015-12-15 17:45:05 +0100

    laneDetection.hpp => Classe Lane faite. laneDetection.cpp => ajoute des constructeurs, du destructeur, et des getteurs&setteurs

--- Programme-detection/laneDetection.cpp
+++ Programme-detection/laneDetection.cpp
@@ -1,1 +1,54 @@
+#include <cv.h>
+#include <highgui.h>
+
+using namespace std;
+
 #include "laneDetection.hpp"
+
+Lane::Lane(unsigned int _x, unsigned int _y)
+:x(_x), y(_y)
+{
+	ID = tempID;
+	tempID = ++tempID;
+}
+
+Lane::Lane()
+{
+	x = 0;
+	y = 0;
+	ID = tempID;
+	tempID = ++tempID;
+}
+
+Lane::~Lane()
+{}
+
+unsigned int Lane::getLaneX()
+{
+	return x;
+}
+
+unsigned int Lane::getLaneY()
+{
+	return y;
+}
+
+long long unsigned int Lane::getLaneID()
+{
+	return ID;
+}
+
+void Lane::setLaneX(unsigned int _x)
+{
+	x = _x;
+}
+
+void Lane::setLaneY(unsigned int _y)
+{
+	y = _y;
+}
+
+void detecLane(IplImage* imgTresh)
+{
+	//à compléter
+}
--- Programme-detection/laneDetection.hpp
+++ Programme-detection/laneDetection.hpp
@@ -4,12 +4,26 @@
 #include <cv.h>
 #include <highgui.h>
 
-class laneDetection 
+class Lane 
 {
-public:
-
 private:
+	unsigned int x,y;
+	long long unsigned int ID;
+	static long long unsigned int tempID = 0;
 
+public:
+	Lane(unsigned int, unsigned int);
+	Lane();
+	~Lane();
+	
+	unsigned int getLaneX();
+	unsigned int getLaneY();
+	long long unsigned int getLaneID();
+
+	void setLaneX(unsigned int);
+	void setLaneY(unsigned int);
+
+	void detecLane(IplImage*);
 };
 
 #endif
