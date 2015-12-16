76eb1feba4881242a5a2c463413be24fff8a69ed
Taehupo <uzumakigip@gmail.com>
2015-12-16 18:41:34 +0100

    test

---
+++ Programme-detection/voiesCirculations.cpp
@@ -0,0 +1,7 @@
+#include <cv.h>
+#include <highgui.h>
+
+using namespace std;
+
+#include "laneDetection.hpp"
+#include "voiesCirculations.hpp"
---
+++ Programme-detection/voiesCirculations.hpp
@@ -0,0 +1,16 @@
+#ifndef VOIESCIRCULATION_HPP
+#define VOIESCIRCULATION_HPP
+
+class voieCirculation
+{
+private:
+	Lane * ligne_1;
+	Lane * ligne_2;
+	unsigned int ID;
+	static unsigned int tempID;
+
+public:
+
+};
+
+#endif
