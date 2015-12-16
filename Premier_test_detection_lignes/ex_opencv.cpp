8033e5c3db66c84a9f55cf57e73f221ca2c43e96
TheSirdragonfly <leon.dubuisson@gmail.com>
2015-12-16 16:01:53 +0100

    Ajout du calcul de la vitesse et update du makefile c++11.

--- Premier_test_detection_lignes/ex_opencv.cpp
+++ Premier_test_detection_lignes/ex_opencv.cpp
@@ -1,6 +1,13 @@
 #include <cv.h>
 #include <highgui.h>
+#include <stdio.h>
+#include <time.h>
+#include <chrono>
+#include <ctime>
+#include <ratio>
+
 using namespace std;
+using namespace std::chrono;
 
 IplImage* imgTracking=0;
 
@@ -9,6 +16,9 @@ int lastY1 = -1;
 
 int lastX2 = -1;
 int lastY2 = -1;
+
+duration<double> time_span;
+high_resolution_clock::time_point t12, t22;
 
 void trackObject(IplImage* imgThresh, int& ni){ //track the possition of the ball
   CvSeq* contour;  //hold the pointer to a contour
@@ -33,17 +43,33 @@ void trackObject(IplImage* imgThresh, int& ni){ //track the possition of the bal
         pt[i] = (CvPoint*)cvGetSeqElem(result, i);
       }
 
-      int posX=( pt[0]->x + pt[1]->x + pt[2]->x )/3;
-      int posY=( pt[0]->y + pt[1]->y + pt[2]->y )/3;
+      int posX=( pt[0]->x + pt[1]->x + pt[2]->x +pt[3]->x )/4;
+      int posY=( pt[0]->y + pt[1]->y + pt[2]->y +pt[3]->y )/4;
       if (posY>400) {
-        if(posX > 300 ){
+        if(posX > 300 && posX < 600 ){
           if(lastX1>=0 && lastY1>=0 && posX>=0 && posY>=0){
             // Draw a red line from the previous point to the current point
             cvLine(imgTracking, cvPoint(posX, posY), cvPoint(posX, posY), cvScalar(0,0,255), 10);
-            cvLine(imgTracking, cvPoint(lastX1, lastY2), cvPoint(posX, posY), cvScalar(0,0,255),0);
+            
+            cvLine(imgTracking, cvPoint(pt[0]->x, pt[0]->y), cvPoint(pt[1]->x, pt[1]->y), cvScalar(0,0,255),4);
+            cvLine(imgTracking, cvPoint(pt[1]->x, pt[1]->y), cvPoint(pt[2]->x, pt[2]->y), cvScalar(0,0,255),4);
+            cvLine(imgTracking, cvPoint(pt[2]->x, pt[2]->y), cvPoint(pt[3]->x, pt[3]->y), cvScalar(0,0,255),4);
+            cvLine(imgTracking, cvPoint(pt[3]->x, pt[3]->y), cvPoint(pt[0]->x, pt[3]->y), cvScalar(0,0,255),4);
+
+            if (lastY1-posY>0) {
+              //temps = (float)(clock()-t1)/CLOCKS_PER_SEC;
+              //t1=clock();
+
+              time_span = duration_cast<duration<double>> (high_resolution_clock::now() - t12);
+              t12 = high_resolution_clock::now();
+
+
+              //std::cout << "New line" << lastY1-posY << " " << temps << " " << time_span.count() << " Vitesse : " << 13/(time_span.count())*3.6<< std::endl;
+              std::cout << "New line" << lastY1-posY <<  " " << time_span.count() << " Vitesse : " << 13/(time_span.count())*3.6<< std::endl;
+            }
           }
 
-          ni++; cout << "++" << endl;
+          //ni++; cout << "++" << endl;
           lastX1 = posX;
           lastY1 = posY;  
         }
@@ -51,7 +77,11 @@ void trackObject(IplImage* imgThresh, int& ni){ //track the possition of the bal
           if(lastX2>=0 && lastY2>=0 && posX>=0 && posY>=0){
             // Draw a blue line from the previous point to the current point
             cvLine(imgTracking, cvPoint(posX, posY), cvPoint(posX, posY), cvScalar(255,0,0), 10);
-            cvLine(imgTracking, cvPoint(lastX2, lastY2), cvPoint(posX, posY), cvScalar(255,0,0),0);
+
+            cvLine(imgTracking, cvPoint(pt[0]->x, pt[0]->y), cvPoint(pt[1]->x, pt[1]->y), cvScalar(255,0,0),4);
+            cvLine(imgTracking, cvPoint(pt[1]->x, pt[1]->y), cvPoint(pt[2]->x, pt[2]->y), cvScalar(255,0,0),4);
+            cvLine(imgTracking, cvPoint(pt[2]->x, pt[2]->y), cvPoint(pt[3]->x, pt[3]->y), cvScalar(255,0,0),4);
+            cvLine(imgTracking, cvPoint(pt[3]->x, pt[3]->y), cvPoint(pt[0]->x, pt[3]->y), cvScalar(255,0,0),4);
           }
 
           lastX2 = posX;
@@ -68,11 +98,20 @@ void trackObject(IplImage* imgThresh, int& ni){ //track the possition of the bal
   cvReleaseMemStorage(&storage);
 }
 
+void traceGrille() {
+  for (unsigned int i=0; i<500; i+=100) {
+    for (unsigned int j=0; j<1000; j+=100) {
+      cvLine(imgTracking, cvPoint(j,0), cvPoint(j,1000), cvScalar(255,255,255),1);
+
+    }
+    cvLine(imgTracking, cvPoint(0,i), cvPoint(1000,i), cvScalar(255,255,255),1);
+  }
+}
 
 int main(){
 	int i=0;
     //load the video file to the memory
-    CvCapture *capture = cvCaptureFromAVI("v.mp4");
+    CvCapture *capture = cvCaptureFromAVI("b.mp4");
 
     if(!capture){
         printf("Capture failure\n");
@@ -95,7 +134,7 @@ int main(){
 
     //iterate through each frames of the video     
     while(true){
-		cout << i << endl;
+		//cout << i << endl;
         frame = cvQueryFrame(capture);
         if(!frame) break;
         frame=cvCloneImage(frame); 
@@ -118,6 +157,7 @@ int main(){
         
         //track the possition of the ball
         trackObject(imgGrayScale, i); // fonction définie plus haut
+        traceGrille();
 
         // Add the tracking image and the frame
         cvAdd(frame, imgTracking, frame); //On superpose les deux pour faire apparaitre les points
@@ -130,7 +170,7 @@ int main(){
         cvReleaseImage(&frame);
 
         //Wait 10mS
-        int c = cvWaitKey(15);//15
+        int c = cvWaitKey(30);//15 30 30 30
         //If 'ESC' is pressed, break the loop
         if((char)c==27 ) break;      
     }
