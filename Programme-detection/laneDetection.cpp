ab8ecfd103e7088e3e181f6c14b42f7376c4db66
Drasanart <mathieu.terrier13@gmail.com>
2016-02-18 18:41:53 +0100

    Modification des commentaire v2

--- Programme-detection/laneDetection.cpp
+++ Programme-detection/laneDetection.cpp
@@ -9,32 +9,32 @@ using namespace std;
 
 #include "laneDetection.hpp"
 
-long long unsigned int Lane::tempID = 0; /** Id temporaire initilisé à 0 permettant d'être donné au différente lignes détectés */
+long long unsigned int Lane::tempID = 0; /*! Id temporaire initilisé à 0 permettant d'être donné au différente lignes détectés */
 
-/*!
-* Constructeur et constructeur de copie de la classe laneDetection.hpp
-*/
+////////
+// Constructeur et constructeur de copie de la classe laneDetection.hpp
+////////
 Lane::Lane(unsigned int _x, unsigned int _y)
 :x(_x), y(_y)
 {
-	ID = tempID; 				/** On affecte l'id temporaire a l'id */
-	tempID = ++tempID;			/** On incremente l'id temporaire de 1 pour la prochaine ligne */
+	ID = tempID; 				/*! On affecte l'id temporaire a l'id */
+	tempID = ++tempID;			/*! On incremente l'id temporaire de 1 pour la prochaine ligne */
 }
 
 Lane::Lane()
 {
-	x = 0;						/** la ligne x est initilisé à 0 par défaut */
-	y = 0;						/** la ligne y est initilisé à 0 par défaut */
-	ID = tempID;				/** On affecte l'id temporaire a l'id */
-	tempID = ++tempID;			/** On incremente l'id temporaire de 1 pour la prochaine ligne */
+	x = 0;						/*! la ligne x est initilisé à 0 par défaut */
+	y = 0;						/*! la ligne y est initilisé à 0 par défaut */
+	ID = tempID;				/*! On affecte l'id temporaire a l'id */
+	tempID = ++tempID;			/*! On incremente l'id temporaire de 1 pour la prochaine ligne */
 }
 
 Lane::~Lane()
 {}
 
-/*!
-* Getter et Setter de la classe LaneDetection.hpp permettant de recuperer les valeurs de X Y et ID et de modifier X et Y
-*/
+///////////
+// Getter et Setter de la classe LaneDetection.hpp permettant de recuperer les valeurs de X Y et ID et de modifier X et Y
+///////////
 unsigned int Lane::getLaneX()
 {
 	return x;
@@ -65,5 +65,5 @@ void Lane::setLaneY(unsigned int _y)
 */
 void detecLane(IplImage* imgTresh)
 {
-	/** à compléter */
+	/*! à compléter */
 }
