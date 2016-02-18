2096eb2e3e5c846633cc2ab98883da5d73a64611
Drasanart <mathieu.terrier13@gmail.com>
2016-02-18 19:10:31 +0100

    Modification des commentaire v4

--- Programme-detection/laneDetection.cpp
+++ Programme-detection/laneDetection.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #include "laneDetection.hpp"
 
 
-long long unsigned int Lane::tempID = 0; /*!< Id temporaire initilisé à 0 permettant d'être donné au différente lignes détectés */
+long long unsigned int Lane::tempID = 0; /*! Id temporaire initilisé à 0 permettant d'être donné au différente lignes détectés */
 
 /**
 * \brief Constreur et Constructeur de copie
@@ -19,51 +19,63 @@ long long unsigned int Lane::tempID = 0; /*!< Id temporaire initilisé à 0 perm
 Lane::Lane(unsigned int _x, unsigned int _y)
 :x(_x), y(_y)
 {
-	ID = tempID; 				/*!< On affecte l'id temporaire a l'id */
-	tempID = ++tempID;			/*!< On incremente l'id temporaire de 1 pour la prochaine ligne */
+	ID = tempID; 				/*! On affecte l'id temporaire a l'id */
+	tempID = ++tempID;			/*! On incremente l'id temporaire de 1 pour la prochaine ligne */
 }
 
 Lane::Lane()
 {
-	x = 0;						/*!< la ligne x est initilisé à 0 par défaut */
-	y = 0;						/*!< la ligne y est initilisé à 0 par défaut */
-	ID = tempID;				/*!< On affecte l'id temporaire a l'id */
-	tempID = ++tempID;			/*!< On incremente l'id temporaire de 1 pour la prochaine ligne */
+	x = 0;						/*! la ligne x est initilisé à 0 par défaut */
+	y = 0;						/*! la ligne y est initilisé à 0 par défaut */
+	ID = tempID;				/*! On affecte l'id temporaire a l'id */
+	tempID = ++tempID;			/*! On incremente l'id temporaire de 1 pour la prochaine ligne */
 }
 
 Lane::~Lane()
 {}
 
 /**
-* \brief Getteur et setter de la classe lane Detection
+* \brief Getter : renvoie x
 */
 unsigned int Lane::getLaneX()
 {
 	return x;
 }
 
+/**
+* \brief Getter : renvoie y
+*/
 unsigned int Lane::getLaneY()
 {
 	return y;
 }
 
+/**
+* \brief Getter : renvoie l'ID
+*/
 long long unsigned int Lane::getLaneID()
 {
 	return ID;
 }
 
+/**
+* \brief Setter : on modifie la valeur de x
+*/
 void Lane::setLaneX(unsigned int _x)
 {
 	x = _x;
 }
 
+/**
+* \brief Setter : on modifie la valeur de y
+*/
 void Lane::setLaneY(unsigned int _y)
 {
 	y = _y;
 }
 
 /**
-* Code permettant la détection des lignes, le but et d'ajouter la valeurs obtenue et incrementer de 1 le total des valeurs
+* \brief Code permettant la détection des lignes, le but et d'ajouter la valeurs obtenue et incrementer de 1 le total des valeurs
 */
 void detecLane(IplImage* imgTresh)
 {
