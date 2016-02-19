808170ea9adfd6bc0f8330c7c32a3f481c312735
Drasanart <mathieu.terrier13@gmail.com>
2016-02-19 17:46:20 +0100

    Documentation de ocvCore.hpp et ocvCore.cpp, ce qui était déja a ét conservé et passer avec doxygen sans pb

--- Programme-detection/ocvCore.cpp
+++ Programme-detection/ocvCore.cpp
@@ -1,33 +1,34 @@
-/**
-*\author Gabriel Aubert - Johann Denoize - Etienne Dubuisson - Mathieu Terrier ;
-*\date 19 Février 2016
-*/
-
 #include <cv.h>
 #include <highgui.h>
 
 #include "ocvCore.hpp"
 
-OcvCore::OcvCore(std::string _filename) // Constructeur
+/**
+* Constructeur : celui-ci ne prend que des const char* et permet la création de la fenetre
+*/
+OcvCore::OcvCore(std::string _filename)
 : fileName(_filename)
 {
-	capture = cvCaptureFromAVI(fileName.c_str()); 					/// Ne prend pas les string, que les const char*
+	capture = cvCaptureFromAVI(fileName.c_str()); 					// Ne prend pas les string, que les const char*
 	if (!capture) {
 		std::cerr << "Erreur de chargement ::capture" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	std::cout << "video" << std::endl;
 
-	frame = cvQueryFrame(capture);								 	/// On récupère la première image de la vidéo
+	frame = cvQueryFrame(capture);								 	// On récupère la première image de la vidéo
 	if (!frame) exit(EXIT_FAILURE);
 	std::cout << "image" << std::endl;
 
 	imgTracking=cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
 	cvZero(imgTracking);
 
-	cvNamedWindow("frame");											/// création d'une fenetre "frame"
+	cvNamedWindow("frame");											// création d'une fenetre "frame"
 }
 
+/**
+* Destructeur : Destruction de la fenetre et arret de la capture
+*/
 OcvCore::~OcvCore()
 {
 	cvReleaseImage(&imgTracking);
@@ -35,21 +36,30 @@ OcvCore::~OcvCore()
 	cvDestroyAllWindows();
 }
 
-void OcvCore::filter() //Applique le filtre sur l'image contenue dans frame
+/**
+* Cette fonction permet d'appliquer le filtre sur l'image dans le contenue de la fenetre
+*/
+void OcvCore::filter() 
 {
-	imgGrayScale = cvCreateImage(cvGetSize(frame),8,1); 						/// création de l'image
-	cvCvtColor(frame, imgGrayScale, CV_BGR2GRAY); 								/// Passage en gris
-	cvThreshold(imgGrayScale, imgGrayScale, 100, 255, CV_THRESH_BINARY_INV); 	/// Aff binaire N/B
+	imgGrayScale = cvCreateImage(cvGetSize(frame),8,1); 						// création de l'image
+	cvCvtColor(frame, imgGrayScale, CV_BGR2GRAY); 								// Passage en gris
+	cvThreshold(imgGrayScale, imgGrayScale, 100, 255, CV_THRESH_BINARY_INV); 	// Aff binaire N/B
 }
 
-void OcvCore::getNewFrame() // On recupère l'image suivante
+/**
+* Cette fonction permet de récuperer l'image suivante
+*/
+void OcvCore::getNewFrame() 
 {
 	frame = cvQueryFrame(capture);
 	if (!frame) exit(EXIT_FAILURE);
 	frame=cvCloneImage(frame);
 }
 
-void OcvCore::showFrameWindow() // On affiche l'image dans une fenetre
+/**
+* Cette fonction permet l'affichage de l'image dans la fenetre
+*/
+void OcvCore::showFrameWindow() 
 {	
 	cvShowImage("frame", frame);
 }
--- Programme-detection/ocvCore.hpp
+++ Programme-detection/ocvCore.hpp
@@ -10,8 +10,8 @@
 
 /**
 * \class OcvCore
-* \brief Classe pour la vidéo
-* Cette classe permet la reconnaissance et la détection des lignes
+* \brief Classe pour la vidéo.\n
+* Cette classe permet la reconnaissance et la détection des lignes.
 */
 class OcvCore {
 public:
@@ -24,12 +24,12 @@ public:
 	void superpose (IplImage* _frame1, IplImage* _frame2);
 private:
 	std::string fileName;
-	CvCapture * capture; 				/// Vidéo
+	CvCapture * capture; 				// Vidéo
 
-	IplImage* frame=0; 					/// Une image de la vidéo
-	IplImage* imgGrayScale; 			/// Image noir/blanc
+	IplImage* frame=0; 					// Une image de la vidéo
+	IplImage* imgGrayScale; 			// Image noir/blanc
 
-	IplImage* imgTracking=0; 			/// Image sur laquelle on place les points qui désignent les lignes détectées (bleu et rouge)
+	IplImage* imgTracking=0; 			// Image sur laquelle on place les points qui désignent les lignes détectées (bleu et rouge)
 
 };
 
