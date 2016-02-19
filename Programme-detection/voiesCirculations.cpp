c666e0610b7ecd226ac4f01661eb60798ddc304f
Taehupo <uzumakigip@gmail.com>
2016-02-19 16:33:19 +0100

    Ajout de la doc pour VoiesCirculations.cpp & hpp

--- Programme-detection/voiesCirculations.cpp
+++ Programme-detection/voiesCirculations.cpp
@@ -11,27 +11,33 @@ using namespace std;
 #include "laneDetection.hpp"
 #include "voiesCirculations.hpp"
 
-unsigned int tempID = 0;
+unsigned int tempID = 0; /// ID temporaire initialisée
 
 voieCirculation::voieCirculation()
 :ligne_1(NULL), ligne_2(NULL)
-{throw "Pas de construction par défaut de voie de circualtion"}
+{throw "Pas de construction par défaut de voie de circualtion"} /// La construction par défaut est interdite
 
 voieCirculation::voieCirculation(Lane * _ligne_1, Lane * _ligne_2)
 :ligne_1(_ligne_1), ligne_2(_ligne_2)
 {
-	ID = tempID;
-	tempID = tempID++;
+	ID = tempID; /// On affecte à la voie l'ID temporaire
+	tempID = tempID++; /// On incrémente l'ID temporaire pour garder le compte
 }
 
 voieCirculation::~voieCirculation()
 {}
 
 Lane * voieCirculation::getLane1()
-{return ligne_1;}
+{
+	return ligne_1; /// Renvoie un pointeur vers la première ligne de la voie
+} 
 
 Lane * voieCirculation::getLane2()
-{return ligne_2;}
+{
+	return ligne_2; /// Renvoie un pointeur vers la deuxième ligne de la voie
+} 
 
 unsigned int voieCirculation::getLaneID()
-{return ID;}
+{
+	return ID; /// Renvoie l'ID de la voie de circulation
+}
--- Programme-detection/voiesCirculations.hpp
+++ Programme-detection/voiesCirculations.hpp
@@ -8,14 +8,20 @@
 #ifndef VOIECIRCULATION_HPP
 #define VOIECIRCULATION_HPP
 
+/**
+* \class voieCirculation
+* \brief Classe pour la fonction F3 "Détection des voies de circulation". \n
+* Cette classe permet la détection de voie de circulation à partir des lignes
+*/
+
 class voieCirculation
 {
 private:
-	Lane * ligne_1;
-	Lane * ligne_2;
-	unsigned int ID;
-	static unsigned int tempID;
-	voieCirculation();
+	Lane * ligne_1; 				/// Elle se compose d'une ligne détectée
+	Lane * ligne_2; 				/// Et d'une autre ligne
+	unsigned int ID;				/// D'une ID pour pouvoir reconnaitre quelle est cette voie
+	static unsigned int tempID;		/// Une ID temporaire pour s'occuper des affectation & compter le nombre de lignes
+	voieCirculation();				
 
 public:
 	voieCirculation(Lane*, Lane*);
