dda9bf33c0f3adfa4bbfbc88d189fb8abefc7b0b
Drasanart <mathieu.terrier13@gmail.com>
2016-02-24 22:06:59 +0100

    Ajout de la classe exception ainsi que sa documentation

---
+++ Programme-detection/myException.cpp
@@ -0,0 +1,23 @@
+/**
+*\author Gabriel Aubert - Johann Denoize - Etienne Dubuisson - Mathieu Terrier ;
+*\date 24 Février 2016
+*/
+
+#include <cv.h>
+#include <highgui.h>
+#include <string>
+#include <exception>
+
+using namespace std;
+
+#include "myException.hpp"
+
+myException()
+{
+	/// A completer
+}
+	
+~myException()
+{
+	/// A completer
+}
---
+++ Programme-detection/myException.hpp
@@ -0,0 +1,24 @@
+/**
+*\file myException.hpp
+*\author Gabriel Aubert - Johann Denoize - Etienne Dubuisson - Mathieu Terrier ;
+*\details Cette classe permet la gestion des exceptions
+*\date 24 Février 2016
+*/
+
+#ifndef MYEXCEPTION_HPP
+#define MYEXCEPTION_HPP
+/**
+* \class myException
+* \brief Une classe d'exception. \n
+* Cette classe permet d'attraper et de gérer les exceptions.
+*/
+class myException
+{
+private:
+
+public:
+	myException();
+	~myException();
+};
+
+#endif
