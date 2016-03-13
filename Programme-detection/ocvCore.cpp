6d37fba1834f621ebe079891e1fc071ca62d5d1f
Drasanart <mathieu.terrier13@gmail.com>
2016-03-13 15:34:29 +0100

    Ajout de trackobject : modification de ocvcore.cpp et .hpp ainsi que de laneDetection.hpp avec déplacement de double centrey

--- Programme-detection/ocvCore.cpp
+++ Programme-detection/ocvCore.cpp
@@ -75,3 +75,146 @@ void traceGrille()/// Cette fonction permet d'afficher un quadrillage sur la vid
 		cvLine(imgTracking, cvPoint(0,i), cvPoint(1000,i), cvScalar(255,255,255),1); // trace les lignes horizontales
 	}
 }
+
+void OcvCore::trackObject()
+{
+	CvSeq* contour;  //Pointeur vers un contour
+	CvSeq* result;   //hold sequence of points of a contour
+	CvMemStorage *storage = cvCreateMemStorage(0); //storage area for all contours
+
+	//Recherche de tout les contours de l'image
+	cvFindContours(imgThresh, storage, &contour, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+	
+	uint lignePos = 0; // Position de la ligne 0 = gauche 1 = droite
+
+
+	//On passe sur tout les contours
+	while(contour)
+	{
+		//obtain a sequence of points of the countour, pointed by the variable 'countour'
+		result = cvApproxPoly(contour, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contour)*0.02, 0);
+
+		//if there are 3 vertices  in the contour and the area of the triangle is more than 100 pixels
+		if(result->total==4 && fabs(cvContourArea(result, CV_WHOLE_SEQ))>100 ) // Détection des lignes (4 cotés) et aire > 100
+		{
+			CvPoint *pt[4];
+			for(int i=0;i<4;i++) // Récupération des coordonnées
+			{
+				pt[i] = (CvPoint*)cvGetSeqElem(result, i);
+			}
+
+			int posX=( pt[0]->x + pt[1]->x + pt[2]->x +pt[3]->x )/4; // centre des points
+			int posY=( pt[0]->y + pt[1]->y + pt[2]->y +pt[3]->y )/4;
+
+			if (posY>400 ) //Si en bas de l'écran et proche de la moyenne (&& (posX>centrey-100 || posX<centrey+100))
+			{ 
+				centrey = (ligne_tab[1].posX + ligne_tab[0].posX + posX)/3; //Calcul du centre entre les lignes | . |
+				bool ligne_ = false;
+				if (posX<centrey )//ligne à gauche (&& posX>centrey-200)
+				{ 
+					lignePos = 0;
+					ligne_ = true;
+				}
+				else if (posX>centrey) //ligne à droite de l'écran ( && posX<centrey+200)
+				{ 
+					lignePos = 1;
+					ligne_ = true;
+				} 
+				
+				if (ligne_)
+				{
+					//Sauvegarde des points
+					ligne_tab[lignePos].utilisee = true;
+
+					ligne_tab[lignePos].points[0].x = pt[0]->x;
+					ligne_tab[lignePos].points[0].y = pt[0]->y;
+					ligne_tab[lignePos].points[1].x = pt[1]->x;
+					ligne_tab[lignePos].points[1].y = pt[1]->y;
+					ligne_tab[lignePos].points[2].x = pt[2]->x;
+					ligne_tab[lignePos].points[2].y = pt[2]->y;
+					ligne_tab[lignePos].points[3].x = pt[3]->x;
+					ligne_tab[lignePos].points[3].y = pt[3]->y;
+				
+					//centre de la ligne
+					ligne_tab[lignePos].posX = posX;
+					ligne_tab[lignePos].posY = posY;
+					//cout << "Aire : " << fabs(cvContourArea(result, CV_WHOLE_SEQ)) << endl; 
+				}
+			}
+
+		} 
+		else if(result->total==3 ) // Détection des triangles (3 cotés)
+		{ 
+			CvPoint *pt[3];
+			
+			for(int i=0;i<3;i++) //Récupération des coordonnées
+			{
+				pt[i] = (CvPoint*)cvGetSeqElem(result, i);
+			}
+
+			//Traçage des triangles
+			if (distance2pt(pt[0], pt[1])<50 && distance2pt(pt[0], pt[1])>20 && 
+			distance2pt(pt[1], pt[2])<50 && distance2pt(pt[1], pt[2])>20 &&
+			distance2pt(pt[2], pt[0])<50 && distance2pt(pt[2], pt[0])>20 ) // On vérifie que les mesures correspondent à un panneau triangulaire (3 cotés entre 50 et 20 pix)
+			{ 
+				cvLine(imgTracking, *pt[0], *pt[1], cvScalar(255,0,0),4);
+				cvLine(imgTracking, *pt[1], *pt[2], cvScalar(255,0,0),4);
+				cvLine(imgTracking, *pt[2], *pt[0], cvScalar(255,0,0),4);
+			}
+		}
+
+
+
+		//obtain the next contour
+		contour = contour->h_next; 
+	}
+  
+for (uint i=0; i<2; i++) //traçage des lignes
+{
+	if (ligne_tab[i].utilisee) 
+	{
+		cvLine(imgTracking, cvPoint(ligne_tab[i].points[0].x, ligne_tab[i].points[0].y), cvPoint(ligne_tab[i].points[1].x, ligne_tab[i].points[1].y), cvScalar(0,(i==1)?255:0,255),4);
+		cvLine(imgTracking, cvPoint(ligne_tab[i].points[1].x, ligne_tab[i].points[1].y), cvPoint(ligne_tab[i].points[2].x, ligne_tab[i].points[2].y), cvScalar(0,(i==1)?255:0,255),4);
+		cvLine(imgTracking, cvPoint(ligne_tab[i].points[2].x, ligne_tab[i].points[2].y), cvPoint(ligne_tab[i].points[3].x, ligne_tab[i].points[3].y), cvScalar(0,(i==1)?255:0,255),4);
+		cvLine(imgTracking, cvPoint(ligne_tab[i].points[3].x, ligne_tab[i].points[3].y), cvPoint(ligne_tab[i].points[0].x, ligne_tab[i].points[3].y), cvScalar(0,(i==1)?255:0,255),4);
+		ligne_tab[i].utilisee = false;
+		
+		if (i==0) 
+		{
+			if (lastY1-ligne_tab[0].posY>0) // Si on change de ligne (sur la même file)
+			{ 
+				time_span = duration_cast<duration<double>> (high_resolution_clock::now() - t12); // Calcul du temps
+				t12 = high_resolution_clock::now(); // Prise du nouveau temps 
+				vitesse_lue = taille_ligne/double(time_span.count())*3.6; //calcul de la vitesse taille_ligne(ligne+noir)/temps*conversion_M/S_Km/h
+
+				if (vitesse_lue<moyenne_vitesse+100) //Calcul de la nouvelle moyenne de vitesse et affichage
+				{  
+					moyenne_vitesse = (moyenne_vitesse + vitesse_lue)/2;
+
+					std::cout << "New line " << lastY1-ligne_tab[0].posY <<  " " << time_span.count() << " Vitesse : " << moyenne_vitesse << " Temps : " << temps << std::endl;
+				}
+			}
+			lastY1 = ligne_tab[0].posY;
+		}
+		else if (i==1) 
+		{
+			if (lastY1-ligne_tab[1].posY>1)// Si on change de ligne (sur la même file) 
+			{ 
+				time_span = duration_cast<duration<double>> (high_resolution_clock::now() - t22); // Calcul du temps
+				t22 = high_resolution_clock::now(); // Prise du nouveau temps 
+				vitesse_lue = taille_ligne/double(time_span.count())*3.6; //calcul de la vitesse taille_ligne(ligne+noir)/temps*conversion_M/S_Km/h
+
+				if (vitesse_lue<moyenne_vitesse+100) //Calcul de la nouvelle moyenne de vitesse et affichage 
+				{ 
+					moyenne_vitesse2 = (moyenne_vitesse2 + vitesse_lue)/2;
+					std::cout << "New line " << lastY1-ligne_tab[1].posY <<  " " << time_span.count() << " Vitesse : " << moyenne_vitesse2 << " Temps : " << temps << std::endl;
+				}
+			}
+			lastY1 = ligne_tab[1].posY;
+		}
+		moyenne_vitesseT = ( moyenne_vitesse + moyenne_vitesse2 )/2;
+	}
+}
+
+  cvReleaseMemStorage(&storage);
+}
--- Programme-detection/ocvCore.hpp
+++ Programme-detection/ocvCore.hpp
@@ -13,6 +13,13 @@
 * \brief Classe pour la vidéo.\n
 * Cette classe permet la reconnaissance et la détection des lignes.
 */
+// Structure S_Ligne nécéssaire à trackObject()
+typedef struct {
+	CvPoint points[4];
+	int posX, posY;
+	bool utilisee = false;
+} S_Ligne;
+
 class OcvCore {
 public:
 
@@ -23,6 +30,8 @@ public:
 	void showFrameWindow();
 	void superpose (IplImage* _frame1, IplImage* _frame2);
 	void traceGrille();
+	// Ajout proto2
+	void trackObject(IplImage* imgThresh, int& ni, const int & temps); // Permet la détéction de l'ensemble des éléments : lignes, panneaux
 private:
 	std::string fileName;
 	CvCapture * capture; 				// Vidéo
@@ -31,7 +40,18 @@ private:
 	IplImage* imgGrayScale; 			// Image noir/blanc
 
 	IplImage* imgTracking=0; 			// Image sur laquelle on place les points qui désignent les lignes détectées (bleu et rouge)
-
-};
+	// ajout proto2
+	double centrey = 300;						/*! Position initiale du centre (entre deux lignes) */
+	S_Ligne ligne_tab[2];						/*! tableau de S_ligne : structure plus haut */
+	duration<double> time_span;					// Sert a tackobject, ne sais pas ou mettre
+	high_resolution_clock::time_point t12, t22;	// Sert aussi a track Object, mesure du temps, pour les frames ?
+	float taille_ligne = 13;					/*! Taille d'une bande */
+	/// Moyenne de la vitesse obtenue  grace a la vitesse lue, par l'addition de moyenne_vitesse et moyenne_vitesse2 pour obtenir la vitesse finale
+	double vitesse_lue;							/*! Vitesse lue par le programme */
+	double moyenne_vitesse = 1;
+	double moyenne_vitesse2 = 1;				
+	double moyenne_vitesseT = 1;
+	int lastY1 = -1;							/*! Le seul et l'unique */
+};			
 
 #endif
